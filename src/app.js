3920760099b6246facb64acf90b7409b4d3d6fa7
ZachZuk <zach.zz.zuk@gmail.com>
2024-11-02 19:46:53 -0600

    made the basic elements for everything
    help me i hate this

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// function for going from the home page to the diagnosis page and also run the python code for diagnosing the plant
+function diagnose(plant) {
+    window.location.href = "diagnosis.html";
+    // create a file with info about diagnosis for other pages to use
+}
+
+// function for switching pages that doesn't need other stuff to be run
+function goToPage(page) {
+    window.location.href = page;
+}
